1afa726b226f1b00a03b5d2004f068d98839da78
Daryl Findlay <darylfindlay@gmail.com>
2025-02-25 14:36:08 +0000

    added navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { geistMono, geistSans } from "@/app/ui/fonts";
+import Navbar from "@/app/ui/nav/nav-bar";
 
 export const metadata: Metadata = {
   title: "Northwest Triathlon Club",
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background`}
       >
+        <Navbar />
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,56 +1,70 @@
 import Image from "next/image";
-import HeroSection from "@/app/ui/hero-section";
+// import HeroSection from "@/app/ui/hero-section";
 import { BrandLink } from "@/app/ui/link";
 
 export default function Home() {
   return (
     <>
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <HeroSection />
-        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start"></main>
-        <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-          <BrandLink
-            href="https://www.facebook.com/north.westtriathlon/"
-            target="_blank"
-          >
-            <Image
-              aria-hidden
-              src="/Facebook_Logo_Secondary.png"
-              alt="Facebook"
-              width={2084}
-              height={2084}
-              className="w-6 h-6"
-            />
-            Facebook
-          </BrandLink>
-          <BrandLink
-            href="https://www.instagram.com/northwest.triathlonclub/"
-            target="_blank"
-          >
-            <Image
-              aria-hidden
-              src="/Instagram_Glyph_White.png"
-              alt="Instagram"
-              width={500}
-              height={500}
-              className="w-6 h-6"
-            />
-            @northwest.triathlonclub
-          </BrandLink>
-          <BrandLink
-            href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-          >
-            <Image
-              aria-hidden
-              src="/Digital_Glyph_White.png"
-              alt="WhatsApp"
-              width={360}
-              height={362}
-              className="w-6 h-6"
-            />
-            WhatsApp
-          </BrandLink>
+      <div className="min-h-screen flex flex-col bg-gray-50 text-brandBlue">
+        {/* <HeroSection /> */}
+        <main className="flex-1 container mx-auto px-4 py-8">
+          <h1 className="text-3xl font-bold">Main Content</h1>
+          <p className="mt-4 text-gray-700">
+            This is the main content section.
+          </p>
+        </main>
+        <footer className="bg-gray-100 p-4 text-center">
+          <ul className="flex justify-between sm:justify-center flex-wrap gap-4">
+            <li>
+              <BrandLink
+                href="https://www.facebook.com/north.westtriathlon/"
+                target="_blank"
+              >
+                <Image
+                  aria-hidden
+                  src="/Facebook_Logo_Primary.png"
+                  alt="Facebook"
+                  width={2084}
+                  height={2084}
+                  className="w-6 h-6"
+                />
+                Facebook
+              </BrandLink>
+            </li>
+            <li>
+              <BrandLink
+                href="https://www.instagram.com/northwest.triathlonclub/"
+                target="_blank"
+              >
+                <Image
+                  aria-hidden
+                  src="/Instagram_Glyph_Gradient.png"
+                  alt="Instagram"
+                  width={500}
+                  height={500}
+                  className="w-6 h-6"
+                />
+                @northwest.triathlonclub
+              </BrandLink>
+            </li>
+            <li>
+              <BrandLink
+                href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+                target="_blank"
+              >
+                <Image
+                  aria-hidden
+                  src="/Digital_Glyph_Green.png"
+                  alt="WhatsApp"
+                  width={360}
+                  height={362}
+                  className="w-6 h-6"
+                />
+                WhatsApp
+              </BrandLink>
+            </li>
+          </ul>
+          <p className="text-xs mt-1">© 2025 - Northwest Triathlon Club</p>
         </footer>
       </div>
     </>
