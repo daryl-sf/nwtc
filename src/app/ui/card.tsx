046eca7ef06797091232bce726bfff9b02ee7d53
Daryl Findlay <darylfindlay@gmail.com>
2025-03-05 21:45:10 +0000

    image added

--- src/app/ui/card.tsx
+++ src/app/ui/card.tsx
@@ -25,7 +25,7 @@ export default function Card({
         alt={image.alt}
         width={image.width}
         height={image.height}
-        className="rounded-t w-full border-b-brandGreen border-b-4"
+        className="rounded-t w-full border-b-brandGreen border-b-4 object-cover"
       />
       <div className="px-2 py-4">
         <h2>{title}</h2>
--- src/app/ui/panels/juniors.tsx
+++ src/app/ui/panels/juniors.tsx
@@ -1,48 +1,61 @@
+import Image from "next/image";
+
 export default function Juniors() {
   return (
     <div className="snap-always snap-center" id="juniors">
-      <div className="relative min-h-screen flex bg-brandBlue">
-        <div className="container max-w-screen-xl mx-auto flex justify-center items-center text-2xl ">
-          <div className="p-4 bg-white rounded-lg text-brandBlue flex flex-col gap-3 text-base mx-4 max-h-[70vh] overflow-scroll no-scrollbar">
-            <h1 className="text-2xl font-extrabold text-brandBlue">Juniors</h1>
-            <p>
-              At NWTC, we are passionate about developing young athletes in a
-              fun, supportive, and safe environment. Our junior section provides
-              structured coaching for aspiring traiathletes, focusing on skill
-              development, fitness, and a love for the sport.
-            </p>
-            <p>
-              Age is not a limiting factor at NWTC. If your child canconfidently
-              swim 100m (4 lengths of a 25m pool) in under two minutes and do so
-              safely, we can work with them - as long as the have the desire to
-              become a triathlete.
-            </p>
-            <p>
-              Our junior club has a strong track record of developing
-              top-quality atheletes. Over the years, we have proudly seen four
-              of our juniors progress to represent Ireland at various
-              international events. At NWTC, we provide the coaching, guidance,
-              and opportunities to help young athletes reach their full
-              potential.
-            </p>
-            <p>
-              At NWTC, we believe in making triathlon as accessbile as
-              apossible. We susidise junior training costs through our senior
-              club to ensure that young atheltes have the best opportunity to
-              develop. Fees are calculated per training block, but as a
-              reference, in 2024, the cost per session was around £3.50.
-            </p>
-            <p>
-              NWTC is fully aligened with Triathlon Ireland, ensuring that our
-              coaching, safeguarding, and safety practices meet the highest
-              standards. Our dedicated, qualified coaches are committed to
-              creating a positive and inclusive training environment.
-            </p>
-            <p>
-              Whether your child is new to triathlon or looking to improve their
-              race performance, NWTC offers a fantastic opportunity to train,
-              learn, and grow in a supportive team setting.
-            </p>
+      <div className="relative min-h-screen flex bg-brandBlue pb-20">
+        <div className="container max-w-screen-xl mx-auto text-2xl pt-4">
+          <div className="flex flex-col justify-center items-center max-h-screen ">
+            <Image
+              src="/bikes-transition.jpg"
+              alt="Bikes in transition area"
+              width={5071}
+              height={3379}
+              className="rounded-lg rounded-b-none w-4/5 h-96 object-cover"
+            />
+            <div className="p-4 bg-white rounded-lg text-brandBlue flex flex-col gap-3 text-base mx- max-h-[70vh] overflow-scroll no-scrollbar rounded-t-none w-4/5 border-t-4 border-brandGreen">
+              <h1 className="text-2xl font-extrabold text-brandBlue">
+                Juniors
+              </h1>
+              <p>
+                At NWTC, we are passionate about developing young athletes in a
+                fun, supportive, and safe environment. Our junior section
+                provides structured coaching for aspiring traiathletes, focusing
+                on skill development, fitness, and a love for the sport.
+              </p>
+              <p>
+                Age is not a limiting factor at NWTC. If your child
+                canconfidently swim 100m (4 lengths of a 25m pool) in under two
+                minutes and do so safely, we can work with them - as long as the
+                have the desire to become a triathlete.
+              </p>
+              <p>
+                Our junior club has a strong track record of developing
+                top-quality atheletes. Over the years, we have proudly seen four
+                of our juniors progress to represent Ireland at various
+                international events. At NWTC, we provide the coaching,
+                guidance, and opportunities to help young athletes reach their
+                full potential.
+              </p>
+              <p>
+                At NWTC, we believe in making triathlon as accessbile as
+                apossible. We susidise junior training costs through our senior
+                club to ensure that young atheltes have the best opportunity to
+                develop. Fees are calculated per training block, but as a
+                reference, in 2024, the cost per session was around £3.50.
+              </p>
+              <p>
+                NWTC is fully aligened with Triathlon Ireland, ensuring that our
+                coaching, safeguarding, and safety practices meet the highest
+                standards. Our dedicated, qualified coaches are committed to
+                creating a positive and inclusive training environment.
+              </p>
+              <p>
+                Whether your child is new to triathlon or looking to improve
+                their race performance, NWTC offers a fantastic opportunity to
+                train, learn, and grow in a supportive team setting.
+              </p>
+            </div>
           </div>
         </div>
       </div>
